5b4d39c2ce415eff935c7ef9e70294a5ea7deb8c
Lidiia <l.potryasaeva@gmail.com>
2021-10-28 18:38:58 +0300

    last changes

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Configuration;
+using System.Collections.Generic;
 
 namespace FileManager
 {
@@ -8,103 +9,246 @@ namespace FileManager
     {
         static void Main(string[] args)
         {
-            int lines = Convert.ToInt32(ConfigurationManager.AppSettings.Get("linesOnPage")); //получаем из конфига количество строк на странице с деревом каталогов
 
+            int lines = Convert.ToInt32(ConfigurationManager.AppSettings.Get("linesOnPage")); //получаем из конфига количество строк на странице с деревом каталогов.
+            
+            // отображаем дерево каталогов для диска С, при первом запуске
             string workDir = @"C:\";
             string filename = "catalog.txt";
-            GetPathTree(workDir, filename);
-
             var linesInFile = File.ReadAllLines(filename);
             var pages = linesInFile.Length / lines;
+            List<string> commands = new ();
 
-            var page = 1; //счет начинаю именно с 1, потому что pages - это целая часть от деления массива на строки. То есть это не массив и там нет 0 элемента.
-            PrintSelectedPage(page, pages, lines, linesInFile);
+            commands.Add(workDir);
+            WritePathTreeToTheFile(workDir, filename);
+            PrintSelectedPageOfTreeCatalog(1, pages, lines, linesInFile);
+            PaginationOfCataloAndCommandHistory(pages, linesInFile, 1, lines);
+            ExecuteCommandFromCommandLine(workDir, "catalog1.txt", lines, commands);
 
-            //Console.TreatControlCAsInput = true;
-            ConsoleKey key; 
-            do
+
+            static void PaginationOfCataloAndCommandHistory(int pages, string[] linesInFile, int page, int lines)
             {
-
-                key = Console.ReadKey(true).Key;
-                if (key == ConsoleKey.RightArrow & page < pages)
+                ConsoleKey key;
+                do
                 {
-                    page ++;
-                    PrintSelectedPage(page, pages, lines, linesInFile);
-                }
-                else if (key == ConsoleKey.LeftArrow & page != 1)
-                {
-                    page --;
-                    PrintSelectedPage(page, pages, lines, linesInFile);
-                }
-                else
-                {
-                    PrintSelectedPage(page, pages, lines, linesInFile);
-                }
+                    key = Console.ReadKey(true).Key;
 
-            } while (key != ConsoleKey.Escape);
+                    if (key == ConsoleKey.RightArrow & page < pages)
+                    {
+                        page++;
+                        PrintSelectedPageOfTreeCatalog(page, pages, lines, linesInFile);
+                    }
+                    else if (key == ConsoleKey.LeftArrow & page != 1)
+                    {
+                        page--;
+                        PrintSelectedPageOfTreeCatalog(page, pages, lines, linesInFile);
+                    }
+                    else if (key == ConsoleKey.Enter)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        PrintSelectedPageOfTreeCatalog(page, pages, lines, linesInFile);
+                    }
 
-            static void PrintSelectedPage(int page, int pages, int lines, string[] linesInFile) {
+                } while (key != ConsoleKey.Escape);
+                if (key == ConsoleKey.Escape)
+                {
+                    Environment.Exit(0);
+                }
+            }
+
+
+            static void ExecuteCommandFromCommandLine(string workDir, string filename, int lines, List<string> commands)
+            {
+                File.Delete(filename); //удаляем файл если он существует от предыдущих запросов
+                do
+                {
+                    Console.WriteLine("enter command");
+                    string command = Console.ReadLine();
+                    commands.Add(command);
+                    string[] commandLine = command.Split(' ');
+                    if (commandLine.Length == 1)
+                    {
+                        Console.WriteLine("Please enter correct command and Path");
+                    }
+                    else
+                    {
+                        switch (commandLine[0].ToLower())
+                        {
+                            case "cd":
+                                try
+                                {
+                                    WritePathTreeToTheFile(commandLine[1], filename);
+                                    var linesInFile1 = File.ReadAllLines(filename);
+                                    var pages = linesInFile1.Length / lines;
+                                    var page = 1;
+                                    PrintSelectedPageOfTreeCatalog(page, pages, lines, linesInFile1);
+                                    PaginationOfCataloAndCommandHistory(pages, linesInFile1, page, lines);
+                                    ExecuteCommandFromCommandLine(workDir, filename, lines, commands);
+                                }
+                                catch (Exception)
+                                {
+                                    continue;
+                                }
+                                break;
+                            case "add":
+                                try
+                                {
+                                    File.Create(commandLine[1]);
+                                    Console.WriteLine($"File {commandLine[1]} is created");
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine(ex.Message);
+                                    continue;
+                                }
+                                break;
+                            case "rm":
+                                try
+                                {
+                                    File.Delete(commandLine[1]);
+                                    Console.WriteLine($"File {commandLine[1]} is deleted");
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine(ex.Message);
+                                    continue;
+                                }
+                                break;
+                            case "i":
+                                try
+                                {
+                                    Console.WriteLine($"File attribute - {File.GetAttributes(commandLine[1])}; Creation time - {File.GetCreationTime(commandLine[1])}; Last access time - {File.GetLastAccessTime(commandLine[1])}"); ;
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine(ex.Message);
+                                    continue;
+                                }
+                                break;
+                            case "idir":
+                                try
+                                {
+                                    Console.WriteLine($"Directory root - {Directory.GetDirectoryRoot(commandLine[1])}; Creation time - {Directory.GetCreationTime(commandLine[1])}; Last access time - {Directory.GetLastAccessTime(commandLine[1])}"); ;
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine(ex.Message);
+                                    continue;
+                                }
+
+                                break;
+                            case "mkdir":
+                                try
+                                {
+                                    Directory.CreateDirectory(commandLine[1]);
+                                    Console.WriteLine($"Directory {commandLine[1]} is created");
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine(ex.Message);
+                                    continue;
+                                }
+                                break;
+                            case "rmdir":
+                                try
+                                {
+                                    Directory.Delete(commandLine[1]);
+                                    Console.WriteLine($"File {commandLine[1]} is deleted");
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine(ex.Message);
+                                    continue;
+                                }
+                                break;
+                            default:
+                                Console.WriteLine($"Couldn't fined command {commandLine[0]}. Please check that you use one of this: cd, add, rm, inf, mkdir, rmdir, infdir.");
+                                break;
+                        }
+                    }
+                } while (!File.Exists(filename));
+            }
+
+
+            static void PrintSelectedPageOfTreeCatalog(int page, int pages, int lines, string[] linesInFile)
+            {
                 Console.Clear();
-
-                if (page == 0)
+                ConsoleRules();
+                if (pages == 0)
                 {
-                    Console.WriteLine("Check your code. You use page number 0 somwhere");
+                    foreach (var item in linesInFile)
+                    {
+                        Console.WriteLine(item);
+                    }
                 }
-                if (page < pages + 1)
+                else if (page < pages + 1)
                 {
                     for (int i = lines * (page - 1); i < lines * page; i++)
                     {
                         Console.WriteLine(linesInFile[i]);
                     }
-                    Console.WriteLine("--------------------------------------------------------------------------------");
                 }
-                else 
+                else
                 {
                     Console.WriteLine($"Check your code. You use page number more that {pages}");
                 }
+                Console.WriteLine("-----------------------------------------------------");
             }
 
-            static void GetPathTree(string directory, string filename)
+            static void WritePathTreeToTheFile(string directory, string filename)  //Данный метод создает при необходимости файл и записывает в него дерево подкаталогов и файлов.
             {
-                var catalogElement = Directory.GetFileSystemEntries(directory);
-
-                File.WriteAllText(filename, directory); // записываем в файл строку и перенос строки
-                File.AppendAllText(filename, Environment.NewLine);
-
-                foreach (var i in catalogElement)
+                try
                 {
-                    try
+                    var catalogElement = Directory.GetFileSystemEntries(directory);  //Получаем список подкатологов и файлов в директории
+                    File.WriteAllText(filename, directory);    // записываем в файл строку c введенной директорией и перенос строки
+                    File.AppendAllText(filename, Environment.NewLine);
+
+
+                    foreach (var i in catalogElement) //записываем в файл строки для каждого подкаталога и файла 
                     {
-                        var elementsInPath = Directory.GetFileSystemEntries(i);
-                        File.AppendAllLines(filename, new[] { "  |  ", "  " + i.Replace("C:\\", "|--") });
-                        foreach (var j in elementsInPath)
+                        try
                         {
-                            try
+                            var elementsInPath = Directory.GetFileSystemEntries(i);
+                            File.AppendAllLines(filename, new[] { "  |  ", "  " + i.Replace("C:\\", "|--") });
+                            foreach (var j in elementsInPath)
                             {
-                                File.AppendAllLines(filename, new[] { "  " + j.Replace((i + "\\"), "|  |--") });
-                            }
-                            catch (Exception)
-                            {
-                                continue;
+                                try
+                                {
+                                    File.AppendAllLines(filename, new[] { "  " + j.Replace((i + "\\"), "|  |--") });
+                                }
+                                catch (Exception)
+                                {
+                                    continue;
+                                }
                             }
                         }
+                        catch (Exception)
+                        {
+                            continue;
+                        }
                     }
-                    catch (Exception)
-                    {
-                        continue;
-                    }
+                }
+                catch (DirectoryNotFoundException ex)
+                {
+                    Console.WriteLine(ex.Message);
                 }
             }
 
+            static void ConsoleRules()
+            {
+                Console.WriteLine("------------------------------------------------------------------------");
+                Console.WriteLine("|  KEY              |  ACTION                                          |");
+                Console.WriteLine("------------------------------------------------------------------------");
+                Console.WriteLine("|LEFT/RIGHT ARROW   | listing catalog to the left.                     |");
+                Console.WriteLine("|UP/DOWN ARROW      | command history.Works only on active command line|");
+                Console.WriteLine("|ENTER              | enter in command line.                           |");
+                Console.WriteLine("|ESC                | exit.                                            |");
+                Console.WriteLine("------------------------------------------------------------------------");
 
-            //Вывод информации о выбранном эллементе
-
-
-
-            //командная строка
-
-
-
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # FileManager
+Use LEFT/RIGHT ARROW for listing catalog to the left.
+Use UP/DOWN ARROW in command line by watching history.
+Use ENTER to enter command line.
 To quit press ESC.
+
+*After first run of programm you will see listing of C:\ catalog. You can move there using left or right arrow.
+*If you want to enter command, press enter key and than enter correct command using table:
+-----------------------------------------------------
+cd *path* : for check files and directories in path 
+add *path*: for add file 
+rm *path*: for delete file
+i *path*: for get file information
+idir *path*: for get directory information
+mkdir *path*:for add directory 
+rmdir *path*:for delete directory
